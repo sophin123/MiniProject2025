6b052445f5251bed9871388e9d2b0dac5e281e47
sophn123 <sophinstha123@gmail.com>
2025-07-06 17:00:58 +1000

    fix mysql connection dropout and configure IP address

--- backend/server.js
+++ backend/server.js
@@ -35,35 +35,43 @@ const ensureUploadDir = (req, res, next) => {
 
 // Enable Cross-Origin Resource Sharing (CORS) for the application
 app.use(cors({
-    origin: '*',  // // Allow all origins (not recommended for production; specify allowed origins instead)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Define allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers in CORS requests
+  origin: '*',  // // Allow all origins (not recommended for production; specify allowed origins instead)
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Define allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers in CORS requests
 }));
 
 app.use("/api/upload", express.static("uploads"));
 
-app.use(express.json({ limit : '10mb'}));
+app.use(express.json({ limit: '10mb' }));
 // app.use('/uploads', express.static('uploads'));
-console.log("User:", process.env.MYSQL_USER);
-console.log("Database :" , process.env.MYSQL_DATABASE);
+// console.log("User:", process.env.MYSQL_USER);
+// console.log("Database :" , process.env.MYSQL_DATABASE);
 
- // MySQL Connection
-  const db = mysql.createConnection({
-    host : process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: "fileshare"
-  })
+// MySQL Connection
+const db = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: "fileshare",
+  waitForConnections: true,
+  connectionLimit: 2,
+  queueLimit: 0
+})
 
-  db.connect(function(err){
-    if (err) throw err;
-    console.log("connection as id " + db.threadId);
-  })
+db.getConnection((err, connection) => {
+  if (err) {
+    console.log("Error Connecting DB", err);
+  }
 
+  console.log("Your connection ID is " + connection.threadId);
 
-  // Create table if not exist
-  try {
-    const createTableQuery = `CREATE TABLE
+  connection.release();
+}
+)
+
+// Create table if not exist
+try {
+  const createTableQuery = `CREATE TABLE
         if NOT EXISTS files (
         id INT PRIMARY KEY auto_increment,
         filename VARCHAR(255) NOT NULL,
@@ -72,11 +80,11 @@ console.log("Database :" , process.env.MYSQL_DATABASE);
         uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   )`
 
-    db.execute(createTableQuery)
-  } catch (error) {
-    console.error("Error creating table", error)
-    
-  }
+  db.execute(createTableQuery)
+} catch (error) {
+  console.error("Error creating table", error)
+
+}
 
 // File Storage Configuration
 const storage = multer.diskStorage({
@@ -91,19 +99,21 @@ const storage = multer.diskStorage({
 })
 
 // Upload files in storage
-const upload = multer({storage, limits: {
-  fileSize: 100 * 1024 * 1024
-}})
+const upload = multer({
+  storage, limits: {
+    fileSize: 100 * 1024 * 1024
+  }
+})
 
 // Upload endpoint
 app.post("/api/upload", ensureUploadDir, upload.single('file'), (req, res) => {
-  const {filename, path: filepath,  mimetype } = req.file
+  const { filename, path: filepath, mimetype } = req.file
 
   const q = 'INSERT INTO files (filename, filepath, filetype) VALUES (?, ?, ?)';
 
   db.query(q, [filename, filepath, mimetype], (err, result) => {
-    if(err) return res.status(500).json(err);
-    res.status(200).json({message : "File Uploaded Successfully"})
+    if (err) return res.status(500).json("Custome Error", err);
+    res.status(200).json({ message: "File Uploaded Successfully" })
   })
 })
 
@@ -112,15 +122,15 @@ app.get("/api/files", (req, res) => {
   const q = 'SELECT * FROM files ORDER BY uploaded_at DESC';
 
   db.query(q, (err, result) => {
-    if(err){
+    if (err) {
       console.error("Database query error:", err);
-      return res.status(500).json({error: "Database error occurred"});
+      return res.status(500).json({ error: "Database error occurred" });
     }
 
-    if(result.length === 0){
+    if (result.length === 0) {
       return res.status(200).json([]);
     }
-    
+
     res.status(200).json(result);
   })
 })
@@ -130,20 +140,20 @@ app.delete("/api/file/:id", (req, res) => {
 
   const q = 'SELECT * FROM files WHERE id = ?';
   db.query(q, [req.params.id], (err, result) => {
-    if(err) return res.status(500).json(err);
+    if (err) return res.status(500).json(err);
 
     const filename = result[0].filename;
     const filepath = result[0].filepath;
     fs.unlink(filepath, (err) => {
-      if(err) console.error(err);
+      if (err) console.error(err);
 
       const q = 'DELETE FROM files WHERE id = ?';
       db.query(q, [req.params.id], (err, result) => {
-        if(err) return res.status(500).json(err);
+        if (err) return res.status(500).json(err);
 
-        res.status(200).json({message : `${filename} Deleted Successfully`})
+        res.status(200).json({ message: `${filename} Deleted Successfully` })
       })
-    })   
+    })
   })
 })
 
@@ -153,16 +163,16 @@ app.get('/api/download/:filename', (req, res) => {
   console.log("file", file);
 
   // This sets the Content-Disposition header
-  res.download(file); 
+  res.download(file);
 });
 
 // Define a simple route
 app.get('/', (req, res) => {
-    res.send({ "message": 'Hello from the backend!' });
-  });
+  res.send({ "message": 'Hello from the backend!' });
+});
 
 // App listening at specific port
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-  
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
