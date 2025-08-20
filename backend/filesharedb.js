9328fc5ddfe38e95ce69a52d7c2f5e4bb1be4ea1
sophin123 <46114283+sophin123@users.noreply.github.com>
2025-08-20 20:13:53 +1000

    added failover credential

--- backend/filesharedb.js
+++ backend/filesharedb.js
@@ -8,8 +8,8 @@ require("dotenv").config();
 
 const dbConfig = {
     host: process.env.MYSQL_HOST,
-    user: "root",
-    password: process.env.MYSQL_ROOT_PASSWORD
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || process.env.MYSQL_ROOT_PASSWORD
 }
 
 let fileshareDb;
